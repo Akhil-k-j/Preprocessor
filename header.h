298799b27962030cc2add716c18c95381ee68f4b
Akhil-k-j <152254040+Akhil-k-j@users.noreply.github.com>
2024-06-16 14:09:53 +0530

    Delete header.h

--- header.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-void header_replace(char *);
-void comment_replace(char *);
-void macro_check(char *);
-int count_word(char *);
-void macro_replacement(char *);
